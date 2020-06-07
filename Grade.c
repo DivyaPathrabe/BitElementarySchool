ae88d6ceb7666d5d2d39bd4bc9bfe7ce2317731c
DivyaPathrabe <66524875+DivyaPathrabe@users.noreply.github.com>
2020-06-07 10:47:00 +0530

    created a Grade.c
    This file print the Grade on the bases of marks on the screen.
    Thank you!

---
+++ Grade.c
@@ -0,0 +1,27 @@
+#include<studio.h>
+int main()
+{
+printf("Enter the marks: ");
+int marks=0;
+scanf("%d",&marks);
+if(marks>=100);
+{
+printf("You are in Grade A");
+}
+if(marks<=84);
+{
+printf("You are in Grade B");
+}
+if(marks<=69);
+{
+printf("You are in Grade C");
+}
+if(marks<=54);
+{
+printf("You are in Grade D");
+}
+if(marks<40);
+{
+printf("You are in Grade F");
+
+}
